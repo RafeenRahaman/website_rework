637b84322ac84b3f39ca0d2ce68a41c9dc371034
Rafeen Rahaman <rafeenziarahaman.com>
2023-08-07 20:51:58 -0400

    commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//About
+let aboutBtn = document.getElementById("aboutBtn")
+function aboutfunction() {
+    window.scrollTo(0, 500)
+}
+aboutBtn.addEventListener("click", aboutfunction)
+///////////
+
+//Academics
+let acadBtn = document.getElementById("acadBtn")
+function acadfunction() {
+    window.scrollTo(0, 2000)
+}
+acadBtn.addEventListener("click", acadfunction)
+///////////
